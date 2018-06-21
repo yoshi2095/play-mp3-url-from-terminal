d171d3150e10dd9774e605220fcb53cfbc287c21
yoshi2095 <arora4.yoshi@gmail.com>
2018-06-21 19:59:30 +0530

    initial

---
+++ play-url.js
@@ -0,0 +1,12 @@
+// online audio taken from: https://kiwi6.com/artists/spritekiwi6/souriez-quand-les-oiseaux-chantent
+// used the hotlink which has the extension .mp3 :D
+
+var Player = require('player');
+
+var player = new Player(
+		'http://k007.kiwi6.com/hotlink/p1924y0omx/Souriez_Quand_les_oiseaux_chantent_.mp3'
+	);
+
+player.play(function(err, player){
+  console.log('playend!');
+});
